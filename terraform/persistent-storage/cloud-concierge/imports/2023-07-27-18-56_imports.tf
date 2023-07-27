8712b39a479cef4f6337f51d707e32afd46195f5
dragondrop.cloud <cloud-concierge@dragondrop.cloud>
2023-07-27 18:56:27 +0000

    build: cloud-concierge results

---
+++ terraform/persistent-storage/cloud-concierge/imports/2023-07-27-18-56_imports.tf
@@ -0,0 +1,8 @@
+import {
+  to = "aws_db_instance.created-outside-of-terraform-workflow"
+  id = "created-outside-of-terraform-workflow"
+}
+import {
+  to = "aws_db_subnet_group.default-vpc-0e011b4a5a571b7e6"
+  id = "default-vpc-0e011b4a5a571b7e6"
+}
